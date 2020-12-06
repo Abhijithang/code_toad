2be41dad335c5978ede00f6322f415d0958bbd1f
wma5283 <wma5283@DESKTOP-VH3VUG9.localdomain>
2020-12-05 23:39:07 -0500

    Student List

--- src/client/components/StudentList.js
+++ src/client/components/StudentList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as ReactBootStrap from "react-bootstrap"
 
 class StudentList extends React.Component {
     constructor() {
@@ -6,12 +7,37 @@ class StudentList extends React.Component {
     }
 
     render() {
-      console.log(this.props);
+        console.log(this.props);
+        const stubStudents = [
+            {name: "William Ma", email: "wma5283@gmail.com", phone: "123-456-789"},          
+            {name: "William Ma", email: "wma5283@gmail.com", phone: "123-456-789"},          
+            {name: "William Ma", email: "wma5283@gmail.com", phone: "123-456-789"},          
+            {name: "William Ma", email: "wma5283@gmail.com", phone: "123-456-789"},          
+        ]
+
+        const renderStudent = (stubStudents, index) => {
+            return (
+                <tr key={index}>
+                    <td>{stubStudents.name}</td>
+                    <td>{stubStudents.email}</td>
+                    <td>{stubStudents.phone}</td>
+                </tr>
+            )
+        }
         return(
             <div>
-                <div>
-                    Student List Page
-                </div>
+                <ReactBootStrap.Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {stubStudents.map(renderStudent)}
+                </tbody>
+                </ReactBootStrap.Table>
             </div>
         )
     }
